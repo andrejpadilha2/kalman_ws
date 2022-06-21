f167ccb201e8d6b67464e3893f5f50ffa2901199
Andre Padilha <andrejpadilha@gmail.com>
2022-06-21 16:40:42 -0300

    minor fix

--- src/kalman_filter_nd/setup.py
+++ src/kalman_filter_nd/setup.py
@@ -27,6 +27,8 @@ setup(
         	'kf_nd_turtlesim_in_xy_v0 = kalman_filter_nd.kf_nd_turtlesim_in_xy_v0:main',
         	'kf_sensor_fusion_turtlesim_in_x_v0 = kalman_filter_nd.kf_sensor_fusion_turtlesim_in_x_v0:main',
         	'kf_sensor_fusion_turtlesim_in_xy_v0 = kalman_filter_nd.kf_sensor_fusion_turtlesim_in_xy_v0:main',
+        	'kf_sensor_fusion_turtlesim_in_xy_control_v0 = kalman_filter_nd.kf_sensor_fusion_turtlesim_in_xy_control_v0:main',
+        	
         ],
     },
 )
